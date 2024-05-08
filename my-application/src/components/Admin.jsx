f8837d02f114b41d6e98f4462f5fb01640911800
kaesuu22 <nicolasruilan30@gmail.com>
2024-05-08 17:27:21 +0800

    my application
    sasa

---
+++ my-application/src/components/Admin.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Admin extends Component {
+  render() {
+    return (
+        <main className="flex-1 bg-gray-100 p-8">
+            <h2 className="text-2xl font-semibold text-center justify-center mb-4">Admin</h2>
+        </main>
+    )
+  }
+}
+
+export default Admin
---
+++ my-application/src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <main className="flex-1 bg-gray-100 p-8">
+    <h2 className="text-2xl text-center justify-center font-semibold mb-4">Dashboard</h2>
+    <p className='text-center justify-center'>Welcome to Dashboard Portal</p>
+
+</main>
+  );
+};
+
+export default Dashboard;
